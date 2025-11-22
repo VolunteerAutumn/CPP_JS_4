0b0f1145f4f8c639eecc8d25014ab7d1f31fe40f
VolunteerAutumn <draqoshaimposter@gmail.com>
2025-11-22 11:37:26 +0200

    Implement dynamic 2D array with row addition
    This file implements a dynamic 2D array in C++ with functions to fill, print, and add a new row at a specified position.

---
+++ classwork/cpp/3.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+// ______ basic functions so my fingers won't fuck up ______
+void FillArray(int* arr, int size) 
+{ 
+	for (int i = 0; i < size; i++) 
+	{ 
+		arr[i] = rand() % 100; 
+	} 
+}
+
+void PrintArray(int** arr, int rows, int cols) 
+{ 
+	for (int i = 0; i < rows; i++) 
+	{ 
+		cout << "|  ";
+		for (int j = 0; j < cols; j++) 
+		{ 
+			cout << setw(3) << arr[i][j] << " "; 
+		} 
+		cout << "  |";
+		cout << "\n"; 
+	} 
+}
+
+// ______ TASK FUNCTIONS ______
+void AddNewLineAtThePosition(int**& arr, int& rows, int cols, int position)
+{
+	if (position < 0 || position > rows) 
+  {
+    return;
+  }
+
+	int** newArr = new int* [rows + 1];
+	for (int i = 0; i < position; i++)
+	{
+		newArr[i] = arr[i];
+	}
+
+	newArr[position] = new int[cols];
+	for (int j = 0; j < cols; j++)
+	{
+		newArr[position][j] = 0;
+	}
+
+	for (int i = position; i < rows; i++)
+	{
+		newArr[i + 1] = arr[i];
+	}
+	delete[] arr;
+	arr = newArr;
+	rows++;
+}
+
+// ______ TEST ______
+int main()
+{
+	int cols = 12;
+	int rows = 4;
+	int position = 0;
+
+	int** A = new int*[rows];
+
+	for (int i = 0; i < rows; i++)
+	{
+		A[i] = new int[cols];
+		FillArray(A[i], cols);
+	}
+
+	cout << "On which position do I add a new row? (0 - 5) >>> ";
+	cin >> position;
+
+	cout << "======================================================" << endl;
+	PrintArray(A, rows, cols);
+	cout << "======================================================" << endl;
+	AddNewLineAtThePosition(A, rows, cols, position-1);
+	PrintArray(A, rows, cols);
+	cout << "======================================================" << endl;
+
+	for (int i = 0; i < rows; i++)
+		delete[] A[i];
+	delete[] A;
+}
